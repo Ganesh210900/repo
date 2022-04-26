0043b561cd7c23c15a5a98e533e1a3aea0884db2
ganes <ganes@DESKTOP-G9FBDHJ>
2022-04-26 18:25:33 +0530

    commit123

---
+++ Git123/src/Orange.java
@@ -0,0 +1,16 @@
+
+public class Orange {
+	String color;
+	Orange()
+	{
+		this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Orange Apple = new Orange("Red");
+		System.out.println("Color of apple is:" +color);
+
+	}
+
+}
